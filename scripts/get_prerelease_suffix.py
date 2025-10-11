c5454f8676ab17580e6e03c1a0b0e14b97f3be65
biaProjects <beatriz.06.06.2016@gmail.com>
2025-10-10 23:04:07 -0300

    Organize the automation script into folder

---
+++ scripts/get_prerelease_suffix.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import re
+import os
+
+
+def set_prerelease_env():
+    
+    github_head_ref = os.getenv('NAME_BRANCH') 
+
+    if not github_head_ref:
+        print("Erro: A variável de ambiente NAME_BRANCH não está definida no ambiente do Job.")
+        return
+
+    name_branch = github_head_ref.lower()
+    
+    prerelease_match = re.findall("alpha|beta|prerelease", name_branch)
+
+    if prerelease_match:
+        prerelease_value = prerelease_match[0]
+        
+        env_file = os.getenv('GITHUB_ENV')
+        
+        if env_file:
+            with open(env_file, "a") as myfile:
+                myfile.write(f"PRERELEASE={prerelease_value}\n") 
+            print(f"Variável PRERELEASE definida com sucesso: {prerelease_value}")
+        else:
+            print("AVISO: Variável de ambiente GITHUB_ENV não encontrada. (Não executando em GitHub Actions?)")
+    else:
+        print("Nenhum status de prerelease encontrado no nome do branch. Nenhuma variável definida.")
+
+
+def main():
+    set_prerelease_env()
+
+
+if _name_ == '_main_':
+    main()
